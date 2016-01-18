a002f30e4f27d35cbb957a5f0772bc007c4fe06c
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-01-18 10:32:17 +1000

    template for view objects

--- OccIDEAS/src/main/java/org/occideas/rest/ModuleService.java
+++ OccIDEAS/src/main/java/org/occideas/rest/ModuleService.java
@@ -1,6 +1,7 @@
 package org.occideas.rest;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -50,9 +51,6 @@ public class ModuleService extends NodeCommon
 			try {
 				session = HibernateUtility.getSessionFactory().openSession();
 				module = (Module) session.get(Module.class, id);
-				for(Node node:module.getChildNodes()){
-					System.out.println(node.getIdNode());
-				}
 			} catch (Throwable e) {
 				throw new GenericException("Database failure.",e);
 			}
@@ -61,12 +59,22 @@ public class ModuleService extends NodeCommon
 		}
 		return Response.ok(module).build();
 	}
+	@SuppressWarnings("unchecked")
 	@GET
 	@Path("/getlist")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getModules()
+	public Response getModules()
 	{
-		return super.getAllModules();
+		List<Module> modules = null;
+		Session session = null;
+		try {
+			session = HibernateUtility.getSessionFactory().openSession();
+			modules = (List<Module>) session.createCriteria(Module.class).list();
+		} catch (Throwable e) {
+			return Response.status(Status.BAD_REQUEST).type("text/plain").entity(e.getMessage()).build();
+		}
+		return Response.ok(modules).build();
+		
 	}
 
 	@PUT
