2b2b0ba309091a96fe409d629080a273116c7e60
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-02-09 18:48:23 +1000

    updated view structure

--- OccIDEAS/data_migration/createviews.sql
+++ OccIDEAS/data_migration/createviews.sql
@@ -1,6 +1,10 @@
-DROP VIEW IF EXISTS ModuleRules;
-CREATE VIEW ModuleRules AS 
-SELECT m.idNode as idModule, nr.idRule, r.agentId as idAgent
+DROP VIEW IF EXISTS ModuleRule;
+CREATE VIEW ModuleRule AS 
+SELECT concat(m.idNode, ':',nr.idRule, ':',r.agentId, ':',n.idNode)  as primaryKey,
+ m.idNode as idModule, 
+ nr.idRule,
+ r.agentId as idAgent, 
+ n.idNode
 FROM Node n
 INNER JOIN Node m ON n.topNodeId = m.idNode 
 INNER JOIN Node_Rule nr ON n.idNode = nr.idNode
