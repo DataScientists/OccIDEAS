4cbdf3eae4ed5db239cc1c7edc7a9cdf875b3f78
jesus-dayo <jesus_dayo@yahoo.com>
2016-02-12 21:31:39 +0800

    (75%) https://datascientists.plan.io/issues/944

---
+++ OccIDEAS/src/main/webapp/scripts/questions/directive/preventContextDirective.js
@@ -0,0 +1,14 @@
+(function(){
+	angular
+	  .module('occIDEASApp.Questions').directive('preventRightClick', function() {
+	    return {
+	    	retrict: 'A',
+	    	link: function($scope,$ele){
+	    		$ele.bind("contextmenu",function(e){
+	    			e.preventDefault();
+	    			e.stopPropagation();
+	    		});
+	    	}
+	    }
+	});
+})();
