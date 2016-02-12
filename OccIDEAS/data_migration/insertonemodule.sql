6fe45bdd8ccc23361bca3e8005d97a7ac10a6a46
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-02-13 09:41:45 +1000

    fix for issue #982 and no data schema for testing

---
+++ OccIDEAS/data_migration/insertonemodule.sql
@@ -0,0 +1,4 @@
+INSERT INTO `occideas`.`Node` (`node_discriminator`, `idNode`, `deleted`, `description`, 
+`lastUpdated`, `link`, `name`, `nodeclass`, `number`, `originalId`, `sequence`, 
+`topNodeId`, `type`) 
+VALUES ('M', '1', '0', '', '', '', 'default', 'M', '', '', '', '', 'M_Module');
