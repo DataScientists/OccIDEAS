958c858456117d6b917ef04b2128a90478a7e855
jesus-dayo <jesus_dayo@yahoo.com>
2016-03-01 09:13:27 +0800

    interview focus

--- OccIDEAS/src/main/webapp/scripts/interviews/interviews.js
+++ OccIDEAS/src/main/webapp/scripts/interviews/interviews.js
@@ -1,15 +1,30 @@
-(function(){
-	angular
-	  .module('occIDEASApp.Interviews',['ui.router'])
-	  .config(Config)
-	  .factory('InterviewsCache',InterviewsCache);
-	
-	Config.$inject = ['$stateProvider'];
-	function Config($stateProvider){
+(function() {
+	angular.module('occIDEASApp.Interviews', [ 'ui.router' ]).config(Config)
+			.factory('InterviewsCache', InterviewsCache).factory(
+					'focus',
+					function($timeout, $window) {
+						return function(id) {
+							$timeout(function() {
+								var element = $window.document
+										.getElementById(id);
+								if (element)
+									element.focus();
+							});
+						};
+					}).directive('eventFocus', function(focus) {
+				return function(scope, elem, attr) {
+						focus(elem[0].id);
+						scope.$on('$destroy', function() {
+						});
+				};
+			});
+
+	Config.$inject = [ '$stateProvider' ];
+	function Config($stateProvider) {
 	}
-	
-	InterviewsCache.$inject = ['$cacheFactory'];
-	function InterviewsCache($cacheFactory){
+
+	InterviewsCache.$inject = [ '$cacheFactory' ];
+	function InterviewsCache($cacheFactory) {
 		return $cacheFactory('interviews-cache');
 	}
 })();
