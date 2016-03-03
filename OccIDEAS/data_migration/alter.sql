875fa0191d1e0fffc1cca8ef0aefb0274641b939
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-03-03 17:05:28 +1000

    set up for saving fired rules and assessments

--- OccIDEAS/data_migration/alter.sql
+++ OccIDEAS/data_migration/alter.sql
@@ -1,3 +1,23 @@
 ALTER TABLE `occideas`.`Interview` 
 ADD COLUMN `fragment_idNode` BIGINT(20) NULL DEFAULT NULL,
 ADD COLUMN `referenceNumber` VARCHAR(255) NOT NULL;
+
+
+CREATE TABLE `occideas`.`Interview_ManualAssessedRules` (
+  `idInterview_ManualAssessedRules` INT NOT NULL AUTO_INCREMENT,
+  `idinterview` INT NULL,
+  `idRule` INT NULL,
+  PRIMARY KEY (`idInterview_ManualAssessedRules`));
+
+  CREATE TABLE `occideas`.`Interview_AutoAssessedRules` (
+  `idInterview_AutoAssessedRules` INT NOT NULL AUTO_INCREMENT,
+  `idinterview` INT NULL,
+  `idRule` INT NULL,
+  PRIMARY KEY (`idInterview_AutoAssessedRules`));
+  
+  
+  CREATE TABLE `occideas`.`Interview_FiredRules` (
+  `idInterview_FiredRules` INT NOT NULL AUTO_INCREMENT,
+  `idinterview` INT NULL,
+  `idRule` INT NULL,
+  PRIMARY KEY (`idInterview_FiredRules`));
