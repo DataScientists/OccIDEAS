0cd62892b8f1d8b4a2a4f7c17538a77e0b40100c
jesus-dayo <jesus_dayo@yahoo.com>
2016-03-30 22:58:50 +0800

    https://datascientists.plan.io/issues/1177

--- OccIDEAS/data_migration/InterviewNewTables.sql
+++ OccIDEAS/data_migration/InterviewNewTables.sql
@@ -1,3 +1,20 @@
+DROP TABLE IF EXISTS `Interview_Question_Answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Interview_Question_Answer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `deleted` int(11) NOT NULL,
+  `interview_idinterview` bigint(20) DEFAULT NULL,
+  `question_idNode` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_pwqvis43ybjtmn8spx8d0rso` (`answer_idNode`),
+  KEY `FK_jxnbqas7e5yffvln5e2e0m8vj` (`interview_idinterview`),
+  KEY `FK_8yoopbue4scj7hoyprv44gdep` (`question_idNode`),
+  CONSTRAINT `FK_8yoopbue4scj7hoyprv44gdep` FOREIGN KEY (`question_idNode`) REFERENCES `Node` (`idNode`),
+  CONSTRAINT `FK_jxnbqas7e5yffvln5e2e0m8vj` FOREIGN KEY (`interview_idinterview`) REFERENCES `Interview` (`idinterview`),
+  CONSTRAINT `FK_pwqvis43ybjtmn8spx8d0rso` FOREIGN KEY (`answer_idNode`) REFERENCES `Node` (`idNode`)
+) ENGINE=InnoDB AUTO_INCREMENT=3397 DEFAULT CHARSET=latin1;
+
 DROP TABLE IF EXISTS `Interview_Question`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -28,6 +45,7 @@ CREATE TABLE `Interview_Answer` (
   `idNode` bigint(20) DEFAULT NULL,
   `name` varchar(2048) DEFAULT NULL,
   `description` varchar(2048) DEFAULT NULL,
+  `answerFreetext` varchar(2048) DEFAULT NULL,
   `nodeClass` varchar(5) DEFAULT NULL,
   `number` varchar(20) DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
