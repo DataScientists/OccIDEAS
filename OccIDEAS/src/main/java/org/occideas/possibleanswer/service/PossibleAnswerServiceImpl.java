9d46259bdb2dee1d115f178f248a0249b134ece9
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-06-02 12:53:49 +1000

    POssibleAnswer Service was missing the transactional annotation which stopped rule node popup from working

--- OccIDEAS/src/main/java/org/occideas/possibleanswer/service/PossibleAnswerServiceImpl.java
+++ OccIDEAS/src/main/java/org/occideas/possibleanswer/service/PossibleAnswerServiceImpl.java
@@ -11,8 +11,10 @@ import org.occideas.mapper.QuestionMapper;
 import org.occideas.vo.PossibleAnswerVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class PossibleAnswerServiceImpl implements PossibleAnswerService {
 
 	@Autowired
