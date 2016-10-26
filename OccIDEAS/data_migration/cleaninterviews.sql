f6ecd40bd9c613b47be4e1942c66068179b7fded
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-10-26 10:50:46 +1000

    new clean test interviews sql script

---
+++ OccIDEAS/data_migration/cleaninterviews.sql
@@ -0,0 +1,3 @@
+DELETE FROM Interview_Question WHERE idinterview NOT IN (SELECT idinterview FROM Interview);
+DELETE FROM Interview WHERE idinterview NOT IN (SELECT distinct idinterview FROM Interview_Question);
+DELETE FROM Participant WHERE idParticipant NOT IN (SELECT distinct idParticipant FROM Interview);
