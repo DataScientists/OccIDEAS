d46e179cca4286984e1f8e0eec351e81690dc94b
mtagapan <marygracetagapan@gmail.com>
2016-12-05 22:00:37 +0800

    Fix case and column name

--- OccIDEAS/data_migration/report.sql
+++ OccIDEAS/data_migration/report.sql
@@ -1,9 +1,9 @@
-ALTER TABLE `occideas`.`report_history` 
-ADD COLUMN `startDt` TIMESTAMP NOT NULL AFTER `updateBy`,
+ALTER TABLE `occideas`.`REPORT_HISTORY` 
+ADD COLUMN `startDt` TIMESTAMP NOT NULL AFTER `updatedBy`,
 ADD COLUMN `endDt` TIMESTAMP NULL AFTER `startDt`,
 ADD COLUMN `duration` FLOAT NULL AFTER `endDt`,
 ADD COLUMN `recordCount` INT(11) NULL AFTER `duration`;
 
 --Populate existing data
-UPDATE `occideas`.`report_history` SET `startDt`='2016-11-04 11:20:56', `duration`='60', recordCount='100';
+UPDATE `occideas`.`REPORT_HISTORY` SET `startDt`='2016-11-04 11:20:56', `duration`='60', recordCount='100';
 
