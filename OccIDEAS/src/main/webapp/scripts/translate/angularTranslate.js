5b4927b27c4ff673e12df8800ade51e72b4d17d6
Troy Sadkowsky <Troy.Sadkowsky@gmail.com>
2016-12-20 10:26:25 +1000

    debugging translations

--- OccIDEAS/src/main/webapp/scripts/translate/angularTranslate.js
+++ OccIDEAS/src/main/webapp/scripts/translate/angularTranslate.js
@@ -12,27 +12,19 @@
     function AsyncLoader($q, $timeout,NodeLanguageService,$sessionStorage){
  		   return function (options) {
  		     var deferred = $q.defer();
- 		     var translations = [];
  		     if(options.key == 'EN'){
  		    	 return translations;
  		     }
  		     
- 		     var lang = _.find($sessionStorage.languages, function(o) { 
- 		    	 return o.language == options.key; 
- 		     });
- 		     if(lang.id){
- 		     NodeLanguageService.getNodeLanguageById(lang.id).then(function(response) {
- 		         if(response.status == '200'){
- 		        	translations = _.map(response.data, function(o){
- 		        	    return {
- 		        	       [o.word]: o.translation
- 		        	    };
- 		        	});
- 		        	return deferred.resolve(translations);
- 		         }else{
- 		        	return deferred.reject(lang.id);
- 		         }
- 		     });
+ 		     if(!$sessionStorage.languages){
+ 		    	NodeLanguageService.getAllLanguage().then(function(response){
+ 					if(response.status == '200'){
+ 						$sessionStorage.languages = response.data;
+ 						 getLanguageFromStorage(options.key,deferred,$sessionStorage,NodeLanguageService);
+ 					}
+ 				})
+ 		     }else{
+ 		    	getLanguageFromStorage(options.key,deferred,$sessionStorage,NodeLanguageService);
  		     }
 // 		     $timeout(function () {
 // 		       deferred.resolve(translations);
@@ -40,6 +32,27 @@
  		  
  		     return deferred.promise;
  		   };
+    }
+    
+    function getLanguageFromStorage(key,deferred,$sessionStorage,NodeLanguageService){
+    	var translations = [];
+    	var lang = _.find($sessionStorage.languages, function(o) { 
+	    	 return o.language == key; 
+	     });
+	    	if(lang.id){
+	 		     NodeLanguageService.getNodeLanguageById(lang.id).then(function(response) {
+	 		         if(response.status == '200'){
+	 		        	translations = _.map(response.data, function(o){
+	 		        	    return {
+	 		        	       [o.word]: o.translation
+	 		        	    };
+	 		        	});
+	 		        	return deferred.resolve(translations);
+	 		         }else{
+	 		        	return deferred.reject(lang.id);
+	 		         }
+	 		     });
+	 		 }
     }
    
 })();
