c573783539f410ebd0e01539ac5c39d79699c22d
mtagapan <marygracetagapan@gmail.com>
2017-01-04 21:10:49 +0800

    Fix new assessment not showing up on assessment tab Found while investigating Task #1739

--- OccIDEAS/src/main/java/org/occideas/participant/dao/ParticipantDao.java
+++ OccIDEAS/src/main/java/org/occideas/participant/dao/ParticipantDao.java
@@ -103,7 +103,7 @@ public class ParticipantDao {
     		+ " and p.idParticipant like :idParticipant"
     		+ " and p.reference like :reference"
     		+ " and p.status like :status"
-    		+ " and i.assessedStatus like :assessedStatus"
+    		+ " and coalesce(i.assessedStatus, '%%') like coalesce(:assessedStatus, '%%')"
     		+ " and i.idinterview like :idinterview"
     		+ " and im.interviewModuleName like :interviewModuleName"
     		+ " and im.idModule != (select value from SYS_CONFIG where name = 'activeintro' limit 1)"
@@ -130,7 +130,7 @@ public class ParticipantDao {
     	    		+ " and p.reference like :reference"
     	    		+ " and p.status like :status"
     	    		+ " and i.idinterview like :idinterview"
-    	    		+ " and i.assessedStatus like :assessedStatus"
+    	    		+ " and coalesce(i.assessedStatus, '%%') like coalesce(:assessedStatus, '%%')"
     	    		+ " and im.interviewModuleName like :interviewModuleName"
     	    		+ " and im.idModule != (select value from SYS_CONFIG where name = 'activeintro' limit 1)"
     	    		+ " and p.deleted = 0";
