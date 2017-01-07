177a10aa54b7ab1b51c946265d8d873929567251
shaneiam <shaneiam@shaneiam-Vostro-230>
2017-01-07 14:57:58 +1000

    remove noise reports

--- OccIDEAS/data_migration/report.sql
+++ OccIDEAS/data_migration/report.sql
@@ -1,9 +1,6 @@
 ALTER TABLE `occideas`.`REPORT_HISTORY` 
-ADD COLUMN `startDt` TIMESTAMP NOT NULL AFTER `updatedBy`,
+ADD COLUMN `startDt` TIMESTAMP NULL AFTER `updatedBy`,
 ADD COLUMN `endDt` TIMESTAMP NULL AFTER `startDt`,
 ADD COLUMN `duration` FLOAT NULL AFTER `endDt`,
 ADD COLUMN `recordCount` INT(11) NULL AFTER `duration`;
-
---Populate existing data
-UPDATE `occideas`.`REPORT_HISTORY` SET `startDt`='2016-11-04 11:20:56', `duration`='60', recordCount='100';
 
