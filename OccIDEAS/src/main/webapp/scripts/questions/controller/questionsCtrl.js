b75c4981ad6e4932117711bb22854e80e49829a2
jesus-dayo <jesus_dayo@yahoo.com>
2017-01-27 06:55:34 +0800

    https://datascientists.plan.io/issues/1861

--- OccIDEAS/src/main/webapp/scripts/questions/controller/questionsCtrl.js
+++ OccIDEAS/src/main/webapp/scripts/questions/controller/questionsCtrl.js
@@ -2622,10 +2622,17 @@
     		   angular.element(document).find('.nodeAgentEnabled').removeClass('nodeAgentEnabled');
     		   angular.element(document).find('.activeNodeAgentEnabled').removeClass('activeNodeAgentEnabled');
     		   $scope.currentToggledNodeWithAgent = undefined;
+    		   $scope.safeApply(function () {
+ 	              $scope.rulesObj.splice(_.findIndex($scope.rulesObj, function(o) { 
+ 	            	  return o.idAgent === agent.idAgent; }), 1)[0];
+    		   });
+    		   agent.style = "";
     		   return;
     	   }
     	   angular.element(document).find('.nodeAgentEnabled').removeClass('nodeAgentEnabled');
     	   agent.active = true;
+			$scope.rulesObj.push(agent);
+			agent.style = "agent-shown";
     	   QuestionsService.getNodesWithAgent(agent.idAgent).then(function(response){
        		if(response.status == '200'){
        			if(response.data[0] == null){
