e16695a7a2de364d22996a4f02ec6baf67306cd1
troy <troy@shadow>
2017-03-23 23:17:39 +1000

    case sensitivity issue on mysql linux os

--- OccIDEAS/data_migration/multiquestion.sql
+++ OccIDEAS/data_migration/multiquestion.sql
@@ -1,8 +1,8 @@
 update Interview_Question
 set type = "Q_multiple" 
-where type is null and question_id in (select idNode from Node where type="Q_multiple")
+where type is null and question_id in (select idNode from Node where type="Q_multiple");
 
-update Interview_Question a, node b
+update Interview_Question a, Node b
 set a.type = b.type
 where 
 question_id = idNode
