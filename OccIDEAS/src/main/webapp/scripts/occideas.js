41e8dd78673a45219b83c6c33abb1a007973c752
jesus-dayo <jesus_dayo@yahoo.com>
2017-04-06 08:52:54 +0800

    fix js error

--- OccIDEAS/src/main/webapp/scripts/occideas.js
+++ OccIDEAS/src/main/webapp/scripts/occideas.js
@@ -96,10 +96,9 @@ angular
     return $delegate;
   });	
 
-   configureDefaults.$inject = ['ngTableDefaults','$state', '$rootScope','AuthenticationService','dataBeanService','$window','$sessionStorage','$q'];
-   function configureDefaults(ngTableDefaults,$state,$rootScope,AuthenticationService,dataBeanService,$window,$sessionStorage,$q) {
+   configureDefaults.$inject = ['ngTableDefaults','$state', '$rootScope','AuthenticationService','dataBeanService','$window','$sessionStorage'];
+   function configureDefaults(ngTableDefaults,$state,$rootScope,AuthenticationService,dataBeanService,$window,$sessionStorage) {
 	   	$rootScope._ = window._; 
-	   	$q.errorOnUnhandledRejections(false);
 	   	ngTableDefaults.params.count = 5;
         ngTableDefaults.settings.counts = [];
         $rootScope.isReadOnly = false; 
