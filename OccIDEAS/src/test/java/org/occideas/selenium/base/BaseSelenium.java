1958266fcae350a187049be976586cc248b543d6
jesus-dayo <jesus_dayo@yahoo.com>
2017-05-03 21:18:07 +0800

    Task #2362

---
+++ OccIDEAS/src/test/java/org/occideas/selenium/base/BaseSelenium.java
@@ -0,0 +1,53 @@
+package org.occideas.selenium.base;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public abstract class BaseSelenium {
+
+	protected static ChromeDriver driver;
+	protected String localhost = "http://localhost:8080/occideas/";
+	
+	protected static void openBrowser(){
+		ClassLoader loader = ClassLoader.getSystemClassLoader();
+		URL path = null;
+		if(SystemUtils.IS_OS_WINDOWS){
+			path = loader.getResource("chromedriver.exe");
+		}else{
+			path = loader.getResource("chromedriver");
+		}
+		System.setProperty("webdriver.chrome.driver",path.getPath());
+		driver = new ChromeDriver(setChromeOptions());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	private static ChromeOptions setChromeOptions() {
+		ChromeOptions copts = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		copts.setExperimentalOption("prefs", prefs);
+		return copts;
+	}
+	
+	protected static void logout() {
+		clickMenubutton();
+		driver.findElement(By.xpath("//*[@id='menu_container_0']/md-menu-content/md-menu-item[3]/a")).click();
+	}
+	
+	protected static void clickMenubutton() {
+		driver.findElement(By.xpath("/html/body/div[2]/header/div/div/ul/li/md-menu/button")).click();
+	}
+	
+	protected void selectModuleTab() {
+		driver.findElement(By.xpath("/html/body/div[2]/div/div/md-content/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[2]")).click();
+	}
+	
+}
--- OccIDEAS/src/test/java/org/occideas/selenium/translation/ModuleLanguageTest.java
+++ OccIDEAS/src/test/java/org/occideas/selenium/translation/ModuleLanguageTest.java
@@ -1,33 +1,63 @@
 package org.occideas.selenium.translation;
 
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.occideas.selenium.base.BaseSelenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 //@Ignore
-public class ModuleLanguageTest {
+public class ModuleLanguageTest extends BaseSelenium{
 	private static final String MODULE_NAME = "test123";
-	private static ChromeDriver driver;
- 	WebElement element;
 	
 	@BeforeClass
-	public static void openBrowser(){
-		ClassLoader loader = ClassLoader.getSystemClassLoader();
-        URL path = loader.getResource("chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver",path.getPath());
-		driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public static void init(){
+		openBrowser();
+	}
+	
+	@AfterClass
+	public static void cleanup(){
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		logout();
+		driver.close();
 	}
 	
 	@Test
+	public void valid_enableLanguageTranslation(){
+		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
+		driver.get(localhost);
+		loginAsAdmin();
+		enableLanguageTranslation();
+		checkLanguageSummaryTabIsOpen();
+		System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName()); 
+	}
+
+	private void checkLanguageSummaryTabIsOpen() {
+		WebElement languageSummaryTab = driver.findElement(By.xpath("/html/body/div[2]/div/div/md-content/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[8]"));
+		Assert.assertNotNull(languageSummaryTab);
+	}
+
+	private void enableLanguageTranslation() {
+		clickMenubutton();
+		WebElement element = (new WebDriverWait(driver, 15))
+				   .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='menu_container_0']/md-menu-content/md-menu-item[1]/button")));
+		element.click();
+		driver.findElement(By.xpath("//*[@id='dialogContent_8']/div/md-content/div/md-checkbox/div[1]")).click();
+		driver.findElement(By.xpath("/html/body/div[5]/md-dialog/form/md-dialog-actions/button[1]")).click();
+		
+	}
+
+	@Ignore
 	public void valid_arabicTranslation(){
 		 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-		 driver.get("http://localhost:8080/occideas/");	
+		 driver.get(localhost);	
 	     loginAsAdmin();
 	     selectModuleTab();
 	     addIntroModule();
@@ -41,9 +71,11 @@ public class ModuleLanguageTest {
 
 	private void addIntroModule() {
 		driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[1]/td/div/button[1]")).click();
+		driver.findElement(By.xpath("//*[@id='id_']/div/div/input")).clear();
 		driver.findElement(By.xpath("//*[@id='id_']/div/div/input")).sendKeys(MODULE_NAME);
-	     driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[2]/td[3]/div/div/input")).sendKeys(MODULE_NAME);
-	     driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[2]/td[5]/button[1]")).click();
+	    driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[2]/td[3]/div/div/input")).clear();
+	    driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[2]/td[3]/div/div/input")).sendKeys(MODULE_NAME);
+	    driver.findElement(By.xpath("//*[@id='tab-content-2']/div/div/ng-view/div/div/table/tbody[1]/tr[2]/td[5]/button[1]")).click();
 	}
 
 	private void loginAsAdmin() {
@@ -52,7 +84,4 @@ public class ModuleLanguageTest {
 	     driver.findElement(By.xpath("//*[@id='login-form']/div[2]/button")).click();
 	}
 
-	private void selectModuleTab() {
-		driver.findElement(By.xpath("/html/body/div[2]/div/div/md-content/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[2]")).click();
-	}
 }
