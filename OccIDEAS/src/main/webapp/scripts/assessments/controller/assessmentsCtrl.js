8d274b75fd8210c5b66fe04ec485d4533e8896c7
bliss <bliss@sl>
2018-02-22 14:02:14 +1000

    fix for assessment status change #4009

--- OccIDEAS/src/main/webapp/scripts/assessments/controller/assessmentsCtrl.js
+++ OccIDEAS/src/main/webapp/scripts/assessments/controller/assessmentsCtrl.js
@@ -13,7 +13,7 @@
 		$scope.$root.tabsLoading = false;
 		$scope.updateButtonDisabled = false;
 		
-		$scope.statuses = ['Not Assessed','Not Reassessed','Incomplete','Needs Review','Finished','Auto Assessed'];				
+		$scope.statuses = ['Not Assessed','Manually Assessed','Incomplete','Needs Review','Finished','Auto Assessed'];				
 		$scope.onChangeSaveStatus = function (idinterview,assessmentStatus){
 			InterviewsService.getInterviewWithRules(idinterview).then(function(response){
 				if(response.status == 200){
