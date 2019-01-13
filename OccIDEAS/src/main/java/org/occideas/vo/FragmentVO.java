707717d1ee6eaf6499ddbfa92a9857ab7560d4a6
troy.sadkowsky@gmail.com <troy.sadkowsky@gmail.com>
2019-01-13 13:33:40 +0600

    fix for note in fired rules for noise ratios adjustments and added ignore unknowns in fragment vo

--- OccIDEAS/src/main/java/org/occideas/vo/FragmentVO.java
+++ OccIDEAS/src/main/java/org/occideas/vo/FragmentVO.java
@@ -3,10 +3,12 @@ package org.occideas.vo;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FragmentVO extends NodeVO{
 
 	@JsonInclude(Include.NON_NULL)
