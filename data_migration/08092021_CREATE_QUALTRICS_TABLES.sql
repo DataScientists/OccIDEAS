1aa47de20c343fa0ee57531c50722a4622872166
jesusdayo <jdayojr@gmail.com>
2021-08-15 09:11:33 +0800

    - modified storing the response as string

--- data_migration/08092021_CREATE_QUALTRICS_TABLES.sql
+++ data_migration/08092021_CREATE_QUALTRICS_TABLES.sql
@@ -1,20 +1,25 @@
+DROP TABLE IF EXISTS QUALTRICS_SURVEY_SUBSCRIPTION;
+
 CREATE TABLE QUALTRICS_SURVEY_SUBSCRIPTION
 (
-    subscriptionId   varchar(2048) NOT NULL,
+    subscriptionId   varchar(255) NOT NULL,
     surveyId         varchar(255) DEFAULT NULL,
     status           varchar(255) DEFAULT NULL,
-    subscriptionDate timestamp     NOT NULL,
+    subscriptionDate timestamp    NOT NULL,
     PRIMARY KEY (subscriptionId)
 );
 
+DROP TABLE IF EXISTS QUALTRICS_SURVEY;
+
 CREATE TABLE QUALTRICS_SURVEY
 (
-    responseId      varchar(2048) NOT NULL,
-    topic           varchar(2048) NOT NULL,
-    brandId         varchar(255)  NOT NULL,
-    surveyId        varchar(255)  NOT NULL,
-    qualtricsStatus varchar(255)  NOT NULL,
-    completedDate   timestamp     NOT NULL,
+    responseId      varchar(255) NOT NULL,
+    topic           varchar(255) NOT NULL,
+    brandId         varchar(255) NOT NULL,
+    surveyId        varchar(255) NOT NULL,
+    qualtricsStatus varchar(255) NOT NULL,
+    completedDate   timestamp    NOT NULL,
     response        TEXT DEFAULT NULL,
+--     isProcessed     BIT DEFAULT NULL,
     PRIMARY KEY (responseId)
 );
